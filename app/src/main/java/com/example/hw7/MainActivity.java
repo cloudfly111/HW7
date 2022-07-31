30c0c80620b23666f22696ed80cd855794544cdd
Chan Shiang-Yun <cloudfly4326@gmail.com>
2022-07-31 20:44:29 +0800

    Initial commit

---
+++ app/src/main/java/com/example/hw7/MainActivity.java
@@ -0,0 +1,164 @@
+package com.example.hw7;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ImageView imageViewCounty,imageViewFood;
+    private int CountyId,FoodId;
+    private Menu menu;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Set ImageView id:
+        //a ImageView to display the picture of county
+        imageViewCounty=(ImageView) findViewById(R.id.imageView_county);
+        //a ImageView to display the picture of food in different country
+        imageViewFood=(ImageView) findViewById(R.id.imageView_food);
+
+        //Set context menu:
+        registerForContextMenu(imageViewCounty);
+        registerForContextMenu(imageViewFood);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.setup,menu);
+        return true;
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        CountyId =item.getItemId();
+        switch (item.getItemId()){
+            case R.id.taipei:
+                imageViewCounty.setImageResource(R.drawable.taipei);
+                break;
+            case R.id.hsinchu:
+                imageViewCounty.setImageResource(R.drawable.hsinchu);
+                break;
+            case R.id.taoyuan:
+                imageViewCounty.setImageResource(R.drawable.taoyuan);
+                break;
+
+        }
+        imageViewFood.setImageResource(R.drawable.ic_launcher_background);
+        return super.onOptionsItemSelected(item);
+    }
+
+
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        if(v.getId()==R.id.imageView_food){
+           getMenuInflater().inflate(R.menu.textview_food,menu);
+            this.menu=menu;
+            updateMenuTitle();
+        }
+    }
+
+    private void updateMenuTitle(){
+        MenuItem item = menu.findItem(R.id.menu_food1);
+        MenuItem item2 = menu.findItem(R.id.menu_food2);
+        MenuItem item3 = menu.findItem(R.id.menu_food3);
+        String[] foodArray = null;
+        switch (CountyId){
+            case R.id.taipei:
+                foodArray=getResources().getStringArray(R.array.taipei_food);
+                break;
+            case R.id.taoyuan:
+                foodArray=getResources().getStringArray(R.array.taoyuan_food);
+                break;
+            case R.id.hsinchu:
+                foodArray=getResources().getStringArray(R.array.hsinchu_food);
+                break;
+        }
+        Log.d("main","food1="+foodArray[0]);
+        Log.d("main","food1="+foodArray[1]);
+        Log.d("main","food1="+foodArray[2]);
+
+        item.setTitle(foodArray[0]);
+        item2.setTitle(foodArray[1]);
+        item3.setTitle(foodArray[2]);
+    }
+
+    @Override
+    public boolean onContextItemSelected(@NonNull MenuItem item) {
+        FoodId=item.getItemId();
+        setFoodPicture(CountyId);
+//        switch (item.getItemId()){
+//            case R.id.menu_food1:
+//                imageViewFood.setImageResource(R.drawable.ice_cream);
+//                break;
+//            case R.id.menu_food2:
+//                imageViewFood.setImageResource(R.drawable.spaghetti);
+//
+//                break;
+//            case R.id.menu_food3:
+//                imageViewFood.setImageResource(R.drawable.cake);
+//
+//                break;
+//
+//        }
+        return super.onContextItemSelected(item);
+    }
+
+    public int setFoodPicture(int countyName){
+        if(countyName==R.id.taipei){
+            switch (FoodId){
+                case R.id.menu_food1:
+                    imageViewFood.setImageResource(R.drawable.tp1);
+                    break;
+                case R.id.menu_food2:
+                    imageViewFood.setImageResource(R.drawable.tp2);
+                    break;
+                case R.id.menu_food3:
+                    imageViewFood.setImageResource(R.drawable.tp3);
+                    break;
+            }
+        }else if(countyName==R.id.taoyuan){
+            switch (FoodId){
+                case R.id.menu_food1:
+                    imageViewFood.setImageResource(R.drawable.t1);
+                    break;
+                case R.id.menu_food2:
+                    imageViewFood.setImageResource(R.drawable.t2);
+                    break;
+                case R.id.menu_food3:
+                    imageViewFood.setImageResource(R.drawable.t3);
+                    break;
+            }
+        }else{
+            switch (FoodId){
+                case R.id.menu_food1:
+                    imageViewFood.setImageResource(R.drawable.h1);
+                    break;
+                case R.id.menu_food2:
+                    imageViewFood.setImageResource(R.drawable.h2);
+                    break;
+                case R.id.menu_food3:
+                    imageViewFood.setImageResource(R.drawable.h3);
+                    break;
+            }
+        }
+        return 0;
+    }
+
+}
